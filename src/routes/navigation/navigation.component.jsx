5d4051a17b33ba7f255d728442998df4a10a235b
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-06 19:57:22 -0300

    Setting Up Firebase

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,10 @@
 import { Fragment } from "react";
-import { Outlet, Link } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
+
 
 
 const Navigation = () => {
@@ -7,11 +12,14 @@ const Navigation = () => {
         <Fragment>
             <div className="navigation">
                 <Link className="logo-container" to='/'>
-                    <div>Logo</div>
+                    <CrwnLogo className="logo"/>
                 </Link>
                 <div className="nav-links-container">
                     <Link className="nav-link" to='/shop'>
                         SHOP
+                    </Link>
+                    <Link className="nav-link" to='/sign-in'>
+                        SIGN IN
                     </Link>
                 </div>
             </div>
