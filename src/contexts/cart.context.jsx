876e6e205b7c451d6b32f6eb77818f16313ce367
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-10 15:43:46 -0300

    Create checkout item - part 1

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -19,11 +19,27 @@ const addCartItem = (cartItems, productToAdd) => {
     return [...cartItems, { ...productToAdd, quantity: 1 }];
 }
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id);
+
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItems.map((cartItem) =>
+            cartItem.id === cartItemToRemove.id 
+            ? { ...cartItem, quantity: cartItem.quantity - 1}
+            : cartItem
+        );
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
+    removeItemToCart: () => {},
     cartCount: 0
 });
 
@@ -42,8 +58,17 @@ export const CartProvider = ({children}) => {
         setCartItems(addCartItem(cartItems, productToAdd))
     }
 
+    const removeItemToCart = (cartItemToRemove) => {
+        setCartItems(removeCartItem(cartItems, cartItemToRemove))
+    }
+
     const value = {
-        isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount
+        isCartOpen, 
+        setIsCartOpen, 
+        addItemToCart, 
+        removeItemToCart, 
+        cartItems, 
+        cartCount
     };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
