c44841c395cbd2ba999b37b204d4454a6e2a3003
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-10 01:19:39 -0300

    Create add to cart - pt2

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -2,12 +2,21 @@ import { createContext, useState } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
     //find if cartItems contains productToAdd 
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === productToAdd.id
+    );
 
     //If found, increment quantity 
-
+    if(existingCartItem) {
+        return cartItems.map((cartItem) =>
+            cartItem.id === productToAdd.id 
+            ? { ...cartItem, quantity: cartItem.quantity + 1}
+            : cartItem
+        );
+    }
 
     //return new array with modified cartItems / new cart item 
-
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
 }
 
 export const CartContext = createContext({
@@ -25,7 +34,7 @@ export const CartProvider = ({children}) => {
         setCartItems(addCartItem(cartItems, productToAdd))
     }
 
-    const value = {isCartOpen, setIsCartOpen};
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems};
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
