8202d407c00afb3fec63d66cfae5bc5f694d9873
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-10 23:34:18 -0300

    Get Products and Categories from Firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
-import PRODUCTS from '../shop-data.json';
+// import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+// import SHOP_DATA from '../shop-data.js';
+
 
 export const ProductsContext = createContext({
     products: [],
 });
 
 export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
+
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', SHOP_DATA)
+    // }, []);
+
+    useEffect(() => {
+        const getCategoriesMap = async() => {
+           const categoryMap = await getCategoriesAndDocuments();
+           console.log(categoryMap);
+        }
+        getCategoriesMap();
+    })
+
+
     const value = {products};
 
     return (
